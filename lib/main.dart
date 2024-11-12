9dc71afccdda197433b09a469735f1d2e194c33b
Abdulrhman Mohammed Shabaan <126196441+AbdulrhmanM0hamed@users.noreply.github.com>
2024-11-12 20:37:37 +0200

    add fire base

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:e_commerce/core/services/shared_preferences.dart';
 import 'package:e_commerce/core/utils/theme/theme.dart';
 import 'package:e_commerce/features/splash/presentation/view/splash_view.dart';
 import 'package:e_commerce/generated/l10n.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+ 
   await Prefs.init();
     runApp(const FruitHub());
 }
