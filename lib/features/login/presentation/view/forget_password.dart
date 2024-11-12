338425fd08258aa12044e3d47c7a494031f63e79
Abdulrhman Mohammed Shabaan <126196441+AbdulrhmanM0hamed@users.noreply.github.com>
2024-11-12 18:31:34 +0200

    finish fprgetpass screen and verfiy screen

--- lib/features/login/presentation/view/forget_password.dart
+++ lib/features/login/presentation/view/forget_password.dart
@@ -1,10 +1,7 @@
 import 'package:e_commerce/core/utils/common/custom_app_bar.dart';
-import 'package:e_commerce/features/login/presentation/view/verification_code_view.dart';
+import 'package:e_commerce/features/login/presentation/view/widgets/forget_password_view_body.dart';
 import 'package:flutter/material.dart';
-import 'package:e_commerce/generated/l10n.dart';
-import 'package:e_commerce/core/utils/common/custom_text_form_field.dart';
-import 'package:e_commerce/core/utils/common/elvated_button.dart';
-import 'package:e_commerce/core/utils/constants/values_manger.dart';
+
 
 class ForgotPasswordView extends StatelessWidget {
   const ForgotPasswordView({super.key});
@@ -14,26 +11,8 @@ class ForgotPasswordView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: customAppBar(context , "نسيان كلمة المرور"),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            CustomTextFormField(
-              hintText: "ادخل رقم الهاتف ", // يجب أن تتأكد من أن هذه الترجمة موجودة في ملف الترجمة.
-              icon: const Icon(Icons.phone),
-            ),
-            const SizedBox(height: AppSize.s20),
-            CustomElevatedButton(
-              onPressed: () {
-                // منطق التنقل إلى صفحة كود التحقق
-                Navigator.pushNamed(context, VerificationCodeView.routeName);
-              },
-              buttonText: "نسيت كلمة المرور "
-            ),
-          ],
-        ),
-      ),
+      body: const ForgetPasswordViewBody(),
     );
   }
 }
+
--- lib/features/login/presentation/view/verification_code_view.dart
+++ lib/features/login/presentation/view/verification_code_view.dart
@@ -1,6 +1,9 @@
+import 'package:e_commerce/core/utils/common/custom_app_bar.dart';
+import 'package:e_commerce/core/utils/common/elvated_button.dart';
+import 'package:e_commerce/core/utils/constants/colors.dart';
+import 'package:e_commerce/core/utils/constants/font_manger.dart';
+import 'package:e_commerce/core/utils/constants/styles_manger.dart';
 import 'package:flutter/material.dart';
-import 'package:e_commerce/generated/l10n.dart';
-import 'package:e_commerce/core/utils/constants/values_manger.dart';
 
 class VerificationCodeView extends StatelessWidget {
   const VerificationCodeView({super.key});
@@ -9,27 +12,70 @@ class VerificationCodeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('التحقق من الرمز'), // يجب أن تتأكد من أن هذه الترجمة موجودة في ملف الترجمة.
-      ),
+      appBar: customAppBar(context, "التحقق من الرمز") ,
       body: Padding(
         padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: List.generate(4, (index) {
-            return SizedBox(
-              width: 60,
-              child: TextField(
-                keyboardType: TextInputType.number,
-                textAlign: TextAlign.center,
-                maxLength: 1,
-                decoration: InputDecoration(
-                  counterText: "", // لإخفاء عداد الحروف
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            );
-          }),
+        child: Column(
+          children: [
+           Text.rich(
+                    TextSpan(
+                      text: "ادخل الرمز الذى أرسلناه إالى عنوان بريد التالى ",
+                      style: getSemiBoldStyle(
+                        fontFamily: FontConstant.cairo,
+                        fontSize: FontSize.size16, // Responsive font size
+                        color: TColors.textSecondary,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: "abdo@gmail.com",
+                          style: getSemiBoldStyle(
+                            fontFamily: FontConstant.cairo,
+                            fontSize: FontSize.size18, // Responsive font size
+                            color: TColors.primary,
+                          ),
+                        ),
+                      ],
+                    ),
+                    textAlign: TextAlign.start, // Aligns text to start
+                    overflow: TextOverflow
+                        .clip, // Allows text to wrap to the next line
+                  ),
+           const   SizedBox(
+                    height: 20,
+                  ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: List.generate(4, (index) {
+                return SizedBox(
+                  width: 80,
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    textAlign: TextAlign.center,
+                    maxLength: 1,
+                    decoration: InputDecoration(
+                      focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(4),
+                        borderSide:const BorderSide(color: Color.fromARGB(255, 231, 186, 102) , )
+                        
+                      ) ,
+                      counterText: "", 
+                      border: OutlineInputBorder(
+                        
+                        borderRadius: BorderRadius.circular(4)
+                      ),
+                    ),
+                    style: getBoldStyle(fontFamily: FontConstant.cairo , fontSize: FontSize.size24),
+                  ),
+                );
+              }),
+            ),
+        const    SizedBox(height: 26,) ,
+  CustomElevatedButton(onPressed: (){}, buttonText: "تحقق من الرمز "),
+ const SizedBox(
+    height: 24,
+  ),
+  Text("إعادة إرسال الرمز" , style: getSemiBoldStyle(fontFamily: FontConstant.cairo,  color: TColors.primary , fontSize: FontSize.size18))
+          ],
         ),
       ),
     );
