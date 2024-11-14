996ac4bc324968c5c2d6a437c59ad8ae0deb57ef
Abdulrhman Mohammed Shabaan <126196441+AbdulrhmanM0hamed@users.noreply.github.com>
2024-11-14 02:00:35 +0200

    add on error Method

--- lib/core/services/custom_bloc_observer.dart
+++ lib/core/services/custom_bloc_observer.dart
@@ -11,6 +11,15 @@ class CustomBlocObserver extends BlocObserver {
     }
   }
 
+@override
+void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+  super.onError(bloc, error, stackTrace);
+  if (kDebugMode) {
+    log('${bloc.runtimeType} $error', name: 'BlocError');
+  }
+}
+
+
   @override
   void onChange(BlocBase bloc, Change change) {
     super.onChange(bloc, change);
@@ -18,4 +27,7 @@ class CustomBlocObserver extends BlocObserver {
       log('${bloc.runtimeType} $change', name: 'BlocChange');
     }
   }
+
+  
 }
+
